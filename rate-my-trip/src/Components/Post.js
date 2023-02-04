0df92316f1c8883b2d7b4a3abb5d1030ac5a8249
Ethan Ogle <encogle@gmail.com>
2023-02-04 05:24:24 -0500

    adding posts

--- rate-my-trip/src/Components/Post.js
+++ rate-my-trip/src/Components/Post.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const Post = ({ post }) => {
   return (
     <div>
@@ -10,13 +9,6 @@ const Post = ({ post }) => {
         {post.end}
       </p>
     </div>
-=======
-import React from 'react'
-
-const Post = (img, desc, user) => {
-  return (
-    <div>Post</div>
->>>>>>> eccf54489ed57d27fb7b3e9ce95e2453f9ab5187
   )
 }
 
