191a28a354b213def75cc43684908b23aa4cae61
katyatokarev <katyaltokarev>
2023-02-04 13:29:21 -0500

    Added cjanged to slide

--- rate-my-trip/src/Components/Slider/slider.js
+++ rate-my-trip/src/Components/Slider/slider.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import {useState} from 'react'
+import {AiOutlineArrowLeft} from "react-icons/ai"
+import {AiOutlineArrowRight} from "react-icons/ai"
+import {sliderData} from "./slider-data"
 import "./Slider.scss"
 
 const Slider = () => {
+    const [currentSlde,setCurrentSlde] = useState(0) //using react hooks
   return (
-    <div>
-        <h1>Slider</h1>
+    <div className="slider">
+       <AiOutlineArrowLeft className= "arrow prev"/>
+       <AiOutlineArrowRight className= "arrow next"/>
+
+       {sliderData.map((slide, index) => {
+            return (
+                //switches between which slide is on the screen 
+                <div className= {index === currentSlide ?
+                 "slide current" : "slide"} key={index}> 
+                    {index === currentSlide && (
+                        <img
+                    )}
+                </div>    
+            )
+       })}
     </div>
   )
 }
