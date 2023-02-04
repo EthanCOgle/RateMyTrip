2d9675ae0e4e452741efd513d4f840542f3b7e57
probro91 <bozdener@gmail.com>
2023-02-04 13:27:05 -0500

    What

---
+++ rate-my-trip/client/src/App.js
@@ -0,0 +1,54 @@
+import NavBar from "./Components/NavBar"
+//import Posts from "./Components/Posts"
+import { useState } from "react"
+import Search from "./pages/Search"
+import Explore from "./pages/Explore"
+import SignIn from "./pages/SignIn"
+import SignUp from "./pages/SignUp"
+import Home from "./pages/Home"
+import {Route, Routes} from "react-router-dom"
+
+function App() {
+  // const [posts, setPosts] = useState([
+  //   {
+  //     id: 1,
+  //     start: "Athens, GA",
+  //     end: "Atlanta, GA",
+  //     image: "insert_image"
+  //   },
+  //   {
+  //     id: 2,
+  //     start: "New York City, NY",
+  //     end: "Dallas, TX",
+  //     image: "insert_image"
+  //   }
+  // ])
+  
+  
+
+  return (
+    <div>
+      <>
+        <NavBar/>
+        <div className = "container">
+          <Routes>
+            <Route path = "/" element = {<Home />} />
+            <Route path = "/explore" element = {<Explore />} />
+            <Route path = "/search" element = {<Search />} />
+            <Route path = "/signUp" element = {<SignUp />} />
+            <Route path = "/signIn" element = {<SignIn />} />
+
+          </Routes>
+          
+
+
+
+
+        </div>
+        {}
+      </>
+    </div>
+  );
+}
+
+export default App;
