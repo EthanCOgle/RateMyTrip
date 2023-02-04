86eda2869dac69ecbd1bbf1795d5ffdfaec5346c
probro91 <bozdener@gmail.com>
2023-02-04 03:41:36 -0500

    The backbone of the website

--- rate-my-trip/src/Components/NavBar.js
+++ rate-my-trip/src/Components/NavBar.js
@@ -1,20 +1,45 @@
 import PropTypes from "prop-types"
 import Button from "./Button"
-
+import {Link, useMatch, useResolvedPath} from "react-router-dom"
 const NavBar = ({ title }) => {
   return (
-    <div>
-      <h1>
-        {title}
-        <Button text="Search"/>
-        <Button text="Explore"/>
-        <Button text="Post"/>
-        <Button color="grey" text="Sign In"/>
-        <Button color="grey" text="Sign Out"/>
-      </h1>
-    </div>
+    <nav className = "nav">
+      <Link to = "/" className = "site_title"> RateMyTrip</Link>
+      <ul>
+       <CustomLink to = "/search">Search</CustomLink>
+       <CustomLink to = "/explore">Explore</CustomLink>
+       <CustomLink to = "/signIn">Sign In</CustomLink>
+       <CustomLink to = "/signUp">Sign Up</CustomLink>
+      </ul>
+    </nav>
+
+    // <div>
+    //   <h1>
+    //     {title}
+    //     <Button text="Search"/>
+    //     <Button text="Explore"/>
+    //     <Button text="Post"/>
+    //     <Button color="grey" text="Sign In"/>
+    //     <Button color="grey" text="Sign Out"/>
+    //   </h1>
+    // </div>
   )
 }
+
+function CustomLink({to, children, ...props}) {
+  const resolvedPath = useResolvedPath(to)
+  const isActive = useMatch({ path: resolvedPath.pathname, end: true })
+  return (
+    <li className={isActive ? "active" : ""}>
+      <Link to = {to} {...props}>
+        {children}
+      </Link>
+    </li>
+  )
+}
+   
+
+
 
 NavBar.defaultProps = {
   title: "Navigation Bar"
